30c5b8b6a8cc548debc6ead67d2b8ab2e31e47af
Alejandro Calderon-Mateos <acaldero.uc3m@gmail.com>
2026-02-15 11:52:14 +0100

    Update install.sh

--- materiales/mcp-jsonrpc/install.sh
+++ materiales/mcp-jsonrpc/install.sh
@@ -5,12 +5,10 @@ echo "Installing fastapi, fastmcp, etc...."
 python3 -m pip install -U fastapi fastmcp httpx starlette uvicorn --break-system-packages
 
 echo "Installing nvm...."
-if [ ! -f $HOME/.nvm/nvm.sh ]; then
-     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-     export NVM_DIR="$HOME/.nvm"
-     [ -s "$NVM_DIR/nvm.sh" ]          && \. "$NVM_DIR/nvm.sh"
-     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ]          && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 echo "Installing nodejs + npm...."
 nvm install --lts
@@ -19,4 +17,3 @@ nvm use --lts
 
 echo "Installing gemini-cli..."
 npm install -g @google/gemini-cli
-
